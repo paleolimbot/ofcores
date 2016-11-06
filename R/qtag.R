8418b978d15acf394dcda4e113c1fa0b3d57e91d
Dewey Dunnington <dewey@fishandwhistle.net>
2016-11-06 12:43:53 -0400

    update auto plot, add rbind, replacecol and replaceval functions

--- R/qtag.R
+++ R/qtag.R
@@ -281,124 +281,106 @@ fortify.qtag <- function(x, skip_aggregate=FALSE, ...) {
   }
 }
 
-#' Autoplot a qualifier/tag structure
+#' Combine qualifier/tag structures
 #'
-#' @param x A \link{qtag} object
-#' @param xvar Column to be used on the x-axis
-#' @param yvar Column to be used on the y-axis
-#' @param facets Column to be used as facetting variable
-#' @param ... Passed on to \code{aes()}
+#' @param ... Objects to combine
 #'
-#' @return A ggplot object
-#'
-#' @importFrom ggplot2 autoplot
+#' @return A \code{qtag.long} object
 #' @export
 #'
 #' @examples
 #' data(pocmaj)
-#' autoplot(qtag(pocmaj, qualifiers=c("core", "depth")))
+#' pocmaj <- qtag(pocmaj, qualifiers=c("core", "depth"))
+#' newrow <- data.frame(core="POC-2", depth=6, Ca=2100, Ti=4100, V=45)
+#' rbind(pocmaj, newrow)
 #'
-autoplot.qtag.long <- function(x, xvar, yvar, facets, ...) {
-  x <- aggregate(x, mean, err=sd(., na.rm = TRUE)/sum(!is.na(.)))
-  qualifiers <- attr(x, "qualifiers")
-  values <- attr(x, "values")
-  mapping <- aes_string(...)
-  types <- sapply(qualifiers, function(qual) class(x[[qual]])[1])
-  numqualifiers <- qualifiers[types %in% c("numeric", "integer")]
-  nonnumqualifiers <- qualifiers[!(qualifiers %in% numqualifiers) & !(qualifiers %in% mapping)]
-  if(numqualifiers > 0) {
-    if(missing(xvar) && missing(yvar)) {
-      yvar <- numqualifiers[length(numqualifiers)]
-      xvar <- values
-    } else if(missing(xvar)) {
-      if(yvar == values) {
-        xvar <- numqualifiers[length(numqualifiers)]
-      } else {
-        xvar <- values
-      }
-    } else if(missing(yvar)) {
-      if(xvar == values) {
-        yvar <- numqualifiers[length(numqualifiers)]
-      } else {
-        yvar <- values
-      }
-    }
-    facet_scales <- "fixed"
-    if(yvar == values) {
-      facet_scales <- "free_y"
-    } else if(xvar == values) {
-      facet_scales <- "free_x"
-    }
-
-    ggfacet <- facet_null()
-    nonnumindex <- length(nonnumqualifiers)
-    if(missing(facets)) {
-      if(length(nonnumqualifiers) > 0) {
-        # use last non-numeric qualifier
-        ggfacet <- facet_wrap(as.formula(paste0("~", nonnumqualifiers[nonnumindex])), scales = facet_scales)
-        nonnumindex <- nonnumindex - 1
-      } else {
-        ggfacet <- facet_null()
-      }
-    } else if(is.null(facets)) {
-      ggfacet <- facet_null()
-    } else if(attr(terms.formula(facets), "response") == 1) {
-      # 2- sided formula
-      ggfacet <- facet_grid(facets, scales = facet_scales)
-      chrfacets <- unlist(lapply(attr(terms.formula(facets), "variables")[-1], deparse))
-      nonnumqualifiers <- nonnumqualifiers[!(nonnumqualifiers %in% chrfacets)]
-      nonnumindex <- length(nonnumqualifiers)
-    } else {
-      # 1-sided formula
-      chrfacets <- unlist(lapply(attr(terms.formula(facets), "variables")[-1], deparse))
-      ggfacet <- facet_wrap(facets, scales = facet_scales)
-      nonnumqualifiers <- nonnumqualifiers[!(nonnumqualifiers %in% chrfacets)]
-      nonnumindex <- length(nonnumqualifiers)
-    }
-
-    if((nonnumindex > 0) && !("colour" %in% names(mapping))) {
-      mapping <- c(mapping, aes_(colour=as.name(nonnumqualifiers[nonnumindex])))
-      nonnumindex <- nonnumindex - 1
-    }
-    if((nonnumindex > 0) && !("shape" %in% names(mapping))) {
-      mapping <- c(mapping, aes_(shape=as.name(nonnumqualifiers[nonnumindex])))
-      nonnumindex <- nonnumindex - 1
-    }
-
-    errorbars <- NULL
-    if("err" %in% names(x)) {
-      if(values == xvar) {
-        nonvalrange <- range(x[yvar])
-        errbarheight <- (nonvalrange[2]-nonvalrange[1]) / 50.0
-        errorbars <- geom_errorbarh(aes_string(xmin=sprintf("%s-%s", xvar, "err"),
-                                               xmax=sprintf("%s+%s", xvar, "err")),
-                                    height=errbarheight)
-      } else if(values == yvar) {
-        nonvalrange <- range(x[xvar])
-        errbarheight <- (nonvalrange[2]-nonvalrange[1]) / 50.0
-        errorbars <- geom_errorbar(aes_string(ymin=sprintf("%s-%s", yvar, "err"),
-                                               ymax=sprintf("%s+%s", yvar, "err")),
-                                    width=errbarheight)
-      }
-    }
-
-    yrev <- scale_y_reverse()
-    if(.is_ad(x[yvar])) {
-      yrev <- NULL
-    }
-    mapping <- c(mapping, aes_(x=as.name(xvar), y=as.name(yvar)))
-    class(mapping) <- "uneval"
-    return(ggplot(x, mapping) + geom_path() + errorbars + geom_point() + ggfacet + yrev)
-  } else {
-    stop("Cannot use autoplot with zero numeric qualifiers")
+rbind.qtag.long <- function(...) {
+  objs <- list(...)
+  qualifiers <- unique(unlist(lapply(objs, attr, "qualifiers")))
+  tags <- unique(unlist(lapply(objs, attr, "tags")))
+  values <- unique(unlist(lapply(objs, attr, "values")))
+  if(length(values) != 1) {
+    stop("Arguments have multiple values columns: ", paste0("'", values, "'", collapse=", "))
   }
+  out <- do.call(plyr::rbind.fill, objs)
+  class(out) <- c("qtag.long", class(out))
+  attr(out, "qualifiers") <- qualifiers
+  attr(out, "values") <- values
+  attr(out, "tags") <- tags
+  return(out)
 }
 
 #' @export
-#' @rdname autoplot.qtag.long
-autoplot.qtag.wide <- function(x, ...) {
-  autoplot(long(x), ...)
+#' @rdname rbind.qtag.long
+rbind.qtag <- function(...) {
+  do.call(rbind.qtag.long, lapply(list(...), long))
 }
+
+
+#' Replace/rename a column in an object
+#'
+#' Essentially a thin convenience wrapper around \code{plyr::rename(x, list(...))},
+#' except \link{qtag} objects have their qualifiers/tags/values attributes properly modified
+#'
+#' @param ... Key/value pairs to replace in the form \code{oldval="newval"}
+#'
+#' @return A copy of the modified object
+#' @export
+#'
+#' @examples
+#' data(pocmaj)
+#' renamecol(pocmaj, Ca="Calcium")
+#' pocmaj2 <- qtag(pocmaj, qualifiers=c("core", "depth"))
+#' pocmaj2 <- renamecol(pocmaj2, Ca="Calcium")
+#' attr(pocmaj2, "values")
+#'
+replacecol <- function(x, ...) UseMethod("renamecol")
+
+#' @export
+#' @rdname replacecol
+replacecol.default <- function(x, ...) plyr::rename(x, list(...))
+#' @export
+#' @rdname replacecol
+replacecol.qtag <- function(x, ...) {
+  replace <- list(...)
+  out <- plyr::rename(x, replace)
+  attr(out, "qualifiers") <- replaceval(attr(x, "qualifiers"), replace)
+  attr(out, "values") <- replaceval(attr(x, "values"), replace)
+  attr(out, "tags") <- replaceval(attr(x, "tags"), replace)
+  class(out) <- class(x)
+  return(out)
+}
+
+
+#' Replace/rename values in a vector
+#'
+#' @param x Vector of values to replace
+#' @param ... Key/value pairs in the form \code{oldvalue="newvalue"}
+#' @param defaultValue A vector of values to use as the default should the value not
+#'   be found in \code{...}
+#'
+#' @return A vector with values replaced
+#' @export
+#'
+#' @examples
+#' x <- c("fish", "fish", "fish", "whistle")
+#' replaceval(x, fish="newfish")
+#' replaceval(x, whistle="newwhistle")
+#' replaceval(x, fish="newfish", defaultValue="not a fish")
+#'
+replaceval <- function(x, ..., defaultValue=x) {
+  replacer <- list(...)
+  replacernames <- names(replacer)
+  if(length(defaultValue) != length(x)) {
+    defaultValue <- rep(defaultValue, length.out=length(x))
+  }
+  sapply(1:length(x), function(i) {
+    v <- x[i]
+    ifelse(v %in% replacernames, replacer[[v]], defaultValue[i])
+  })
+}
+
+
 
 
 # private for now, generates the lazyeval-like behaviour of the aggregate functions
